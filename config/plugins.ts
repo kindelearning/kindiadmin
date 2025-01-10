85f1c17b3e62c8b761b1ca0ce0d1e733d6d62682
Kindi Learning <KINDI.learn@gmail.com>
2025-01-10 13:30:00 +0530

    Google Plugin added, no packages just API keys in Plugin.js

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,11 +4,22 @@ module.exports = ({ env }) => ({
   email: {
     provider: "sendgrid",
     providerOptions: {
-      apiKey: env("SENDGRID_API_KEY"), // Replace with your SendGrid API key
+      apiKey: env(
+        "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"
+      ), // Replace with your SendGrid API key
     },
     settings: {
-      defaultFrom: "noreply@buildingandflats.com", // Replace with your email
-      defaultReplyTo: "support@kindilearning.com", // Replace with your email
+      defaultFrom: "support@kindi.education", // Replace with your email
+      defaultReplyTo: "support@kindi.education", // Replace with your email
+    },
+  },
+  'users-permissions': {
+    enabled: true,
+    config: {
+      google: {
+        clientId: '1055268493431-9l6ee7s7d7o14dfg5vhh9pr0jgq7rlh7.apps.googleusercontent.com',
+        clientSecret: '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',
+      },
     },
   },
 });
