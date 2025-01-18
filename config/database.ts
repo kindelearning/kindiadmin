5a866bfdff30422a958fa90332bf081b604a7f89
Kindi Learning <KINDI.learn@gmail.com>
2025-01-18 18:38:48 +0530

    Pushing for VPS actions

--- config/database.ts
+++ config/database.ts
@@ -58,3 +58,35 @@ export default ({ env }) => {
     },
   };
 };
+
+
+// export default ({ env }) => {
+//   const client = env("DATABASE_CLIENT", "postgres");
+
+//   const connections = {
+//     postgres: {
+//       connection: {
+//         host: env("DATABASE_HOST", "db.qqtusiblccnjquqsslbh.supabase.co"),
+//         port: env.int("DATABASE_PORT", 5432),
+//         database: env("DATABASE_DATABASE", "postgres"),
+//         user: env("DATABASE_USER", "postgres"),
+//         password: env("DATABASE_PASSWORD", "mrtuRO7rntu3A2zt"),
+//         ssl: env.bool("DATABASE_SSL", false)
+//           ? { rejectUnauthorized: false }  // Enables SSL based on env var
+//           : false,
+//       },
+//       pool: {
+//         min: 2,
+//         max: 10,
+//       },
+//       acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+//     },
+//   };
+
+//   return {
+//     connection: {
+//       client,
+//       ...connections[client],
+//     },
+//   };
+// };
--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['hvbhktocOSsXjZNUDwVywg==','n9tfLUSPgOErrNyeP96iNA==','uv+X6pRR9w+BFsixKHSNNg==','2aCU5mVlFDDg7KZUcbMJbg==']),
+    // keys: env.array('APP_KEYS', ['yourKey1', 'yourKey2']),
   },
 });
